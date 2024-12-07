053fb60a6a0aed0869aced1953de9b04f158a962
Emir Vardar <emir.vardar38@gmail.com>
2024-12-07 23:55:52 +0300

    Proje dosyası ekle.

---
+++ BerberSalonu/Controllers/HizmetController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BerberSalonu.Controllers
+{
+    public class HizmetController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BerberSalonu/Controllers/RandevuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BerberSalonu.Controllers
+{
+    public class RandevuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
